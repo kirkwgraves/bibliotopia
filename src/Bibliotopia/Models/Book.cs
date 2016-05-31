d04475e9902e00b7ada0bd27411eab398d503f63
Kirk Walker Graves <graves.kirk@gmail.com>
2016-05-31 09:38:57 -0700

    Created models for Book and Reading Nook

---
+++ src/Bibliotopia/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bibliotopia.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Synopsis { get; set; }
+        public int Isbn { get; set; }
+        public string ReviewUrl { get; set; }
+    }
+}
---
+++ src/Bibliotopia/Models/ReadingNook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bibliotopia.Models
+{
+    public class ReadingNook
+    {
+        public List<Book> ReadBooks { get; set; }
+        public List<Book> BooksToRead { get; set; }
+    }
+}
