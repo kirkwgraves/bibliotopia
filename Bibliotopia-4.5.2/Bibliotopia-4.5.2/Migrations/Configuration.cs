257b086a26a1e96a5309c8fa860751da825a990e
Kirk Walker Graves <graves.kirk@gmail.com>
2016-06-03 14:37:33 -0700

    Added primary keys for RadingNook, FavoriteBook, and BookToRead models and enabled migrations

---
+++ Bibliotopia-4.5.2/Bibliotopia-4.5.2/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Bibliotopia_4._5._2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Bibliotopia_4._5._2.DAL.BibliotopiaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Bibliotopia_4._5._2.DAL.BibliotopiaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- Bibliotopia-4.5.2/Bibliotopia-4.5.2/Models/ReadingNook.cs
+++ Bibliotopia-4.5.2/Bibliotopia-4.5.2/Models/ReadingNook.cs
@@ -8,23 +8,26 @@ namespace Bibliotopia_4._5._2.Models
 {
     public class ReadingNook
     {
+        public virtual int ReadingNookId { get; set; }
         public virtual ApplicationUser Id { get; set; }
-        public virtual FavoriteBook FavoriteBook { get
+        public virtual IQueryable<FavoriteBook> FavoriteBook { get
             {
                 var context = new BibliotopiaContext();
                 IQueryable<FavoriteBook> favorites_query =
                     from faves in context.FavoriteBooks
                     where faves.Id == faves.ReadingNook.Id
                     select faves;
+                return favorites_query;
             }
         }
-        public virtual BookToRead BookToRead { get
+        public virtual IQueryable<BookToRead> BookToRead { get
             {
                 var context = new BibliotopiaContext();
                 IQueryable<BookToRead> books_to_read_query =
                     from books in context.BooksToRead
                     where books.Id == books.ReadingNook.Id
                     select books;
+                return books_to_read_query;
             }
         }
 
