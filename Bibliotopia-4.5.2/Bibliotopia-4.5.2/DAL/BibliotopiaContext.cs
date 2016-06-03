4c5850abfdb85e081c9b22b211476e361ac788fc
Kirk Walker Graves <graves.kirk@gmail.com>
2016-06-02 21:44:53 -0700

    Created context class and DAL

---
+++ Bibliotopia-4.5.2/Bibliotopia-4.5.2/DAL/BibliotopiaContext.cs
@@ -0,0 +1,17 @@
+﻿using Bibliotopia_4._5._2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bibliotopia_4._5._2.DAL
+{
+    public class BibliotopiaContext : ApplicationDbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<ReadingNook> ReadingNooks { get; set; }
+        public DbSet<FavoriteBook> FavoriteBooks { get; set; }
+        public DbSet<BookToRead> BooksToRead { get; set; }
+    }
+}
