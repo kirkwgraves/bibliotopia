0e463fc4e25f48c689b6f4ded8159bb5bf1135c9
Kirk Walker Graves <graves.kirk@gmail.com>
2016-06-05 13:23:19 -0700

    Wrote TestInitialize method to set up repository tests

--- Bibliotopia-4.5.2/Bibliotopia-4.5.2.Tests/DAL/BibliotopiaRepositoryTests.cs
+++ Bibliotopia-4.5.2/Bibliotopia-4.5.2.Tests/DAL/BibliotopiaRepositoryTests.cs
@@ -18,12 +18,12 @@ namespace Bibliotopia_4._5._2.Tests.DAL
         BibliotopiaRepository repo { get; set; }
 
         // ReadingNooks
-        List<ReadingNook> readingNook_datasource { get; set; }
+        List<ReadingNook> readingNooks_datasource { get; set; }
         Mock<DbSet<ReadingNook>> mock_readingNooks_table { get; set; }
         IQueryable<ReadingNook> readingNook_data { get; set; }
 
         // FavoriteBooks
-        List<FavoriteBook> favoriteBook_datasource { get; set; }
+        List<FavoriteBook> favoriteBooks_datasource { get; set; }
         Mock<DbSet<FavoriteBook>> mock_favoriteBooks_table { get; set; }
         IQueryable<FavoriteBook> favoriteBook_data { get; set; }
 
@@ -32,8 +32,33 @@ namespace Bibliotopia_4._5._2.Tests.DAL
         Mock<DbSet<BookToRead>> mock_booksToRead_table { get; set; }
         IQueryable<BookToRead> bookToRead_data { get; set; }
 
-        // 
+        // Books
+        List<Book> books_datasource { get; set; }
+        Mock<DbSet<Book>> mock_books_table { get; set; }
+        IQueryable<Book> book_data { get; set; }
 
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<BibliotopiaContext>();
+
+            readingNooks_datasource = new List<ReadingNook>();
+            favoriteBooks_datasource = new List<FavoriteBook>();
+            booksToRead_datasource = new List<BookToRead>();
+            books_datasource = new List<Book>();
+
+            mock_readingNooks_table = new Mock<DbSet<ReadingNook>>();
+            mock_favoriteBooks_table = new Mock<DbSet<FavoriteBook>>();
+            mock_booksToRead_table = new Mock<DbSet<BookToRead>>();
+            mock_books_table = new Mock<DbSet<Book>>();
+
+            repo = new BibliotopiaRepository(mock_context.Object);
+
+            readingNook_data = readingNooks_datasource.AsQueryable();
+            favoriteBook_data = favoriteBooks_datasource.AsQueryable();
+            bookToRead_data = booksToRead_datasource.AsQueryable();
+            book_data = books_datasource.AsQueryable();
+        }
 
         [TestMethod]
         public void TestMethod1()
