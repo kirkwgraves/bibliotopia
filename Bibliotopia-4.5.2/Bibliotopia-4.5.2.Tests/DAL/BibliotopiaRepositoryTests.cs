92aefd782bb05b3bd6d5963ca26aa2efea7603d3
Kirk Walker Graves <graves.kirk@gmail.com>
2016-06-05 13:12:21 -0700

    Installed EF6 package in Unit Test and began setting up tests by mocking models and datasources

--- Bibliotopia-4.5.2/Bibliotopia-4.5.2.Tests/DAL/BibliotopiaRepositoryTests.cs
+++ Bibliotopia-4.5.2/Bibliotopia-4.5.2.Tests/DAL/BibliotopiaRepositoryTests.cs
@@ -1,16 +1,38 @@
 ﻿using System;
+using System.Data.Entity;
 using Moq;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bibliotopia_4._5._2.DAL;
+using Bibliotopia_4._5._2.Models;
+using System.Collections.Generic;
 
 namespace Bibliotopia_4._5._2.Tests.DAL
 {
     [TestClass]
     public class BibliotopiaRepositoryTests
     {
-        public BibliotopiaContext context { get; set; }
-       
+        Mock<BibliotopiaContext> mock_context { get; set; }
 
+        // Injects mocked BibliotopiaContext
+        BibliotopiaRepository repo { get; set; }
+
+        // ReadingNooks
+        List<ReadingNook> readingNook_datasource { get; set; }
+        Mock<DbSet<ReadingNook>> mock_readingNooks_table { get; set; }
+        IQueryable<ReadingNook> readingNook_data { get; set; }
+
+        // FavoriteBooks
+        List<FavoriteBook> favoriteBook_datasource { get; set; }
+        Mock<DbSet<FavoriteBook>> mock_favoriteBooks_table { get; set; }
+        IQueryable<FavoriteBook> favoriteBook_data { get; set; }
+
+        // BooksToRead
+        List<BookToRead> booksToRead_datasource { get; set; }
+        Mock<DbSet<BookToRead>> mock_booksToRead_table { get; set; }
+        IQueryable<BookToRead> bookToRead_data { get; set; }
+
+        // 
 
 
         [TestMethod]
