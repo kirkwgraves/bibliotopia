a72a26d44c217f41dd1fd46f8a5299746a652359
Kirk Walker Graves <graves.kirk@gmail.com>
2016-06-10 08:17:13 -0700

    Added ReadingNook Action Method in Home Controller and created NookCtrl and ReadingNook Angular view to handle response for client

--- Bibliotopia-4.5.2/Bibliotopia-4.5.2/Controllers/HomeController.cs
+++ Bibliotopia-4.5.2/Bibliotopia-4.5.2/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Bibliotopia_4._5._2.DAL;
 using System.Web.Mvc;
 
 namespace Bibliotopia_4._5._2.Controllers
@@ -13,18 +15,17 @@ namespace Bibliotopia_4._5._2.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult GetUserReadingNook()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var user_id = User.Identity.GetUserId();
+            var nook_context = new BibliotopiaContext();
+            var selected_nook = nook_context.ReadingNooks.FirstOrDefault(n => n.Owner.Id == user_id);
+            if ((selected_nook != null))
+            {
+                return View(selected_nook);
+            }
+            return View()
         }
     }
+
 }
---
+++ Bibliotopia-4.5.2/Bibliotopia-4.5.2/Scripts/NookCtrl.js
@@ -0,0 +1,19 @@
+﻿(function () {
+
+  angular.module("Bibliotopia").controller("NookCtrl", function ($http) {
+    var vm = this;
+    vm.nook = null;
+    vm.checkForUserNook = function () {
+      $http.get("/Home/GetUserReadingNook")
+        .success(function (response) {
+          if (response.ReadingNook !== null) {
+            vm.nook = response.ReadingNook;
+          }
+          else {
+            vm.nook = null;
+          }
+        });
+    }
+  });
+
+})();
