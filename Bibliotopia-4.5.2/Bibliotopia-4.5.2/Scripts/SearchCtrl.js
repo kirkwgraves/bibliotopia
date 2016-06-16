f1fff69892ae2bc00c8bcc680b345d8f16645468
Kirk Walker Graves <graves.kirk@gmail.com>
2016-06-15 18:23:12 -0700

    Added search functionality for author and subject

--- Bibliotopia-4.5.2/Bibliotopia-4.5.2/Scripts/SearchCtrl.js
+++ Bibliotopia-4.5.2/Bibliotopia-4.5.2/Scripts/SearchCtrl.js
@@ -3,23 +3,43 @@
   angular.module("Bibliotopia").controller("SearchCtrl", function ($http) {
 
     var vm = this;
-    vm.searchText = null;
+    vm.searchTitleText = null;
+    vm.searchAuthorText = null;
+    vm.searchSubjectText = null;
     vm.bookTitleList = null;
 
     vm.searchByTitle = function () {
 
-      $http.get('https://www.googleapis.com/books/v1/volumes?q=' + vm.searchText)
+      $http.get('https://www.googleapis.com/books/v1/volumes?q=' + vm.searchTitleText + '&maxResults=40')
       .success(function (response) {
         vm.bookTitleList = response.items;
         console.log(vm.bookTitleList);
         return response.items;
-        
-        // 	vm.bookTitleList =  response.items;
-        // }).error(function(error) {
-        // 	console.log('Error: ', Error);
       });
     };
 
-  });
 
+    vm.searchByAuthor = function () {
+
+      $http.get('https://www.googleapis.com/books/v1/volumes?q=inauthor:' + vm.searchAuthorText + '&maxResults=40')
+      .success(function (response) {
+        vm.bookTitleList = response.items;
+        console.log(vm.bookTitleList);
+        return response.items;
+      });
+    };
+
+    vm.searchBySubject = function () {
+
+      $http.get('https://www.googleapis.com/books/v1/volumes?q=subject:' + vm.searchSubjectText + '&maxResults=40')
+      .success(function (response) {
+        vm.bookTitleList = response.items;
+        console.log(vm.bookTitleList);
+        return response.items;
+      });
+    };
+
+
+  });
 })();
+  
